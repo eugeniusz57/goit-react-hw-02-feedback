11100efd2ee218db8446d570f8e77d3dc5261068
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-10-18 18:07:40 +0200

    task 2-4

---
+++ src/components/App/App.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Container } from "./App.styled";
+import  {Statistics}  from '../Statistics/Statistics';
+import {FeedbackOptions} from "../FeedbackOptions/FeedbackOptions";
+import { Sections } from "../Section/Section";
+import { Notification } from "../Notification/Notification";
+
+export class App extends React.Component{
+  state = {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    };
+
+    feedbackIncrement = e => {
+        const name = e.target.name;
+        this.setState(prevState => {
+          return {
+            [name]: prevState[name] + 1,
+          };
+        });
+      };
+
+  countTotalFeedback = () =>  (this.state.good + this.state.bad + this.state.neutral);
+  countPositiveFeedbackPercentage = () => (this.state.good / this.countTotalFeedback() * 100);
+
+
+render(){
+      return(
+
+          <div
+    style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 40,
+      color: '#010101'
+    }}
+  >
+<Container>
+    <Sections title="Please leave feedback">
+       <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.feedbackIncrement}/>
+    </Sections>   
+
+    <Sections title="Satatistics">
+        {this.countTotalFeedback() ? <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/> : <Notification message="There is no feedback"/>}
+      
+    </Sections>  
+      
+</Container>
+
+  </div>
+ 
+      );
+  }
+}
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Section = styled.section`
+padding: 20px 5px;
+`
+
+export const SectionsTitle = styled.h2`
+font-size: 28px;
+margin: 10px 0;
+`
